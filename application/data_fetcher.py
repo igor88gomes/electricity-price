9c25a381b6e8e7053cf4f3a72e98e827b4a00bc5
Igor Gomes <igor88gomes@gmail.com>
2025-12-29 13:37:47 +0100

    tests(data_fetcher): align test coverage with current behavior

--- application/data_fetcher.py
+++ application/data_fetcher.py
@@ -2,32 +2,9 @@ import requests
 
 
 def get_elpris_data_from_api(api_url):
-    """
-    Hämta elprisdata från en API URL.
-
-    Denna funktion utför en HTTP GET-begäran till den angivna
-    API URL:en och returnerar JSON-responsdata om begäran
-    lyckas. Om begäran misslyckas eller responsens statuskod
-    inte är inom området 200, hanterar funktionen undantag och
-    returnerar None.
-
-    Args:
-        api_url (str): En sträng som representerar API URL:en att
-        begära data från.
-
-    Returns:
-        dict or None: En dictionary med JSON-responsdata om begäran
-        lyckas, annars None.
-    """
     try:
-        # Skicka en HTTP GET-begäran till API URL:en
         response = requests.get(api_url)
-
-        # Kasta ett undantag om statuskoden i responsen inte är inom området 200
         response.raise_for_status()
-
-        # Returnera JSON-responsdata
         return response.json()
     except requests.exceptions.RequestException:
-        # Hantera undantag och returnera None om hämtning av data misslyckas
         return None
